a7c6dbf9338b88e04bf49e7e0fd322b55d2203c3
zhenhuaizeng <zengzhenhuai8@gmail.com>
2023-10-12 10:17:03 -0700

    added comments and Javadocs to SaasProjectApplication and SaasProjectApplicationTests.java

--- src/main/java/edu/greenriver/sdev/saasproject/SaasProjectApplication.java
+++ src/main/java/edu/greenriver/sdev/saasproject/SaasProjectApplication.java
@@ -1,3 +1,12 @@
+
+/**
+ *  Saasproejctapplication Class
+ *
+ * @author zhenhuai zeng
+ * @version Java 21
+ * Date: 10/12/2023
+ */
+
 package edu.greenriver.sdev.saasproject;
 
 import org.springframework.boot.SpringApplication;
--- src/test/java/edu/greenriver/sdev/saasproject/SaasProjectApplicationTests.java
+++ src/test/java/edu/greenriver/sdev/saasproject/SaasProjectApplicationTests.java
@@ -1,3 +1,13 @@
+/**
+ *  test class
+ *
+ * @author zhenhuai zeng
+ * @version Java 21
+ * Date: 10/12/2023
+ */
+
+
+
 package edu.greenriver.sdev.saasproject;
 
 import org.junit.jupiter.api.Test;
