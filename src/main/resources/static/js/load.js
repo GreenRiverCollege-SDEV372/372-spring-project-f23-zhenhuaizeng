765370de63cecb9acefc8915a6feb8488653d890
zhenhuaizeng <zengzhenhuai8@gmail.com>
2023-11-06 10:22:00 -0800

    games and behaviors table are created.

--- src/main/resources/static/js/load.js
+++ src/main/resources/static/js/load.js
@@ -1,6 +1,6 @@
 window.onload = async function(){
     await fetchGames();
-    /* await fetchBehavior();*/
+    await fetchBehavior();
 }
 
 async function fetchGames()
@@ -28,11 +28,31 @@ async function fetchBehavior()
 
     let response = await fetch(uri,config);
     let json = await response.json();
-    let section = document.getElementById("table");
+    let section = document.querySelector("table");
+    let idTitle = document.createElement("th");
+    let dateTitle = document.createElement("th");
+    dateTitle.setAttribute("type","date");
+    let BehaviorTitle = document.createElement("th");
+    let purchaseTitle = document.createElement("th");
+    let priceTitle = document.createElement("th");
+    let hourseTitle = document.createElement("th");
+    idTitle.innerText = "ID";
+    BehaviorTitle.innerText = "Behavior Name";
+    purchaseTitle.innerText = "Purchase";
+    priceTitle.innerText = "Price";
+    hourseTitle.innerText = "HoursPlayedAverage"
+    section.appendChild(idTitle);
+    section.appendChild(dateTitle);
+    section.appendChild(BehaviorTitle);
+    section.appendChild(purchaseTitle);
+    section.appendChild(priceTitle);
+    section.appendChild(hourseTitle);
+    section.appendChild(document.createElement("tr"));
     for(let i = 0; i < json.length; i ++)
     {
         let element = json[i];
         addBehavior(element,section);
+
     }
 }
 function addElement(element, section)
@@ -62,19 +82,23 @@ function addElement(element, section)
 function addBehavior(behavior,section)
 {
     /*Game Objects*/
-    let id  = document.createElement("p");
-    let name = document.createElement("p");
-    let purchase = document.createElement("p");
-    let price = document.createElement("p");
-    let hoursplayedaverage = document.createElement("p");
-    id.innerText = "ID:" + behavior.id;
-    name.innerText = "Behavior' name: " + behavior.name;
-    purchase.innerText = "purchase: " + behavior.purchase;
-    price.innerText = "price: $" + behavior.price;
-    hoursplayedaverage.innerText = "hoursplayedaverage: " + behavior.hoursplayedaverage;
+    let id  = document.createElement("td");
+    let name = document.createElement("td");
+    let purchase = document.createElement("td");
+    let price = document.createElement("td");
+    let hoursplayedaverage = document.createElement("td");
+    let date = document.createElement("input");
+    date.setAttribute("type","date");
+    id.innerText = behavior.id;
+    name.innerText = behavior.name;
+    purchase.innerText = behavior.purchase;
+    price.innerText = "$" + behavior.price;
+    hoursplayedaverage.innerText = behavior.hoursplayedaverage;
     section.appendChild(id);
     section.appendChild(name);
+    section.appendChild(date);
     section.appendChild(purchase);
     section.appendChild(price);
     section.appendChild(hoursplayedaverage);
+    section.appendChild(document.createElement("tr"));
 }
